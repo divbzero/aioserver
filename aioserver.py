825ceefaa0eaa888a1f721398f116f820fbe57df
Chris Lei <chris@divbzero.com>
2018-02-23 23:59:45 -0800

    NFC: Rename {value => body} for clarity

--- aioserver.py
+++ aioserver.py
@@ -32,38 +32,40 @@ class Application(web.Application):
         return wrapped_handler
 
     def make_response(self, value):
+        # extract <status>, <headers>, <body> from value
         if isinstance(value, int):
             # format: <status>
             # example: return 500
-            status, headers, value = value, {}, None
+            status, headers, body = value, {}, None
         elif not isinstance(value, tuple):
             # format: <body>
             # example: return {'message': 'Hello, World!'}
-            status, headers, value = 200, {}, value
+            status, headers, body = 200, {}, value
         elif len(value) == 2:
             # format: <status>, <body>
             # example: return 404, {'message': 'Not Found'}
-            status, value = value
+            status, body = value
             headers = {}
         elif len(value) == 3:
             # format: <status>, <headers>, <body>
             # example: return 302, {'Location': 'https://www.example.com/'}, {'message': 'Found'}
-            status, headers, value = value
+            status, headers, body = value
         else:
             raise TypeError()
-        if value is None:
+        # return web response
+        if body is None:
             return web.Response(status=status, headers=headers)
-        elif isinstance(value, web.Response):
-            value.set_status(status)
-            value.headers.update(headers)
-            return value
-        elif isinstance(value, bytes):
-            return web.Response(status=status, headers=headers, body=value)
-        elif isinstance(value, str):
-            return web.Response(status=status, headers=headers, text=value)
-        elif isinstance(value, (dict, list)):
+        elif isinstance(body, web.Response):
+            body.set_status(status)
+            body.headers.update(headers)
+            return body
+        elif isinstance(body, bytes):
+            return web.Response(status=status, headers=headers, body=body)
+        elif isinstance(body, str):
+            return web.Response(status=status, headers=headers, text=body)
+        elif isinstance(body, (dict, list)):
             with io.StringIO() as stream:
-                json.dump(value, stream, ensure_ascii=False, allow_nan=False, indent=4, sort_keys=True)
+                json.dump(body, stream, ensure_ascii=False, allow_nan=False, indent=4, sort_keys=True)
                 stream.write('\n')
                 return web.Response(status=status, headers=headers, text=stream.getvalue(), content_type='application/json')
         else:
