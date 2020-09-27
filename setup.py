5edf537b9aedb4d8594daabd7ea7b172642df0e3
Chris Lei <chris@divbzero.com>
2020-09-26 18:47:26 -0700

    Update setup {py_modules => packages}

--- setup.py
+++ setup.py
@@ -76,11 +76,11 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    #   packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    packages=['aioserver'],
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
-    py_modules=['aioserver'],
+    #  py_modules=['aioserver'],
 
     # List run-time dependencies here.  These will be installed by pip when
     # your project is installed. For an analysis of "install_requires" vs pip's
